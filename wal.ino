8d2316bdda9568daf4dbfd04a61030cefb56867f
Mr Fussion <mgsilva@gmail.com>
2021-12-23 13:06:34 -0300

    mejoras en cuentas y manejos de variables, agregados de informacion

--- wal.ino
+++ wal.ino
@@ -6,20 +6,24 @@
 #include <PubSubClient.h>
 #include "OTA.h"
 
+// Tiempo de actualizacion en segundos
+#define LOOP_TIME 60
+
 // Pines
 const int PinTrig = 5;
 const int PinEcho = 4;
 
 // Mqtt
-char mqtt_broker[] = "192.168.1.8";
+char mqtt_broker[] = "broker.emqx.io";
 int mqtt_port = 1883;
 char mqtt_client[] = "wal";
-char mqtt_user[] = "wal";
-char mqtt_pwd[] = "wal2021";
+char mqtt_user[] = "";
+char mqtt_pwd[] = "";
 
-// Medidas del tanque expresada en metros
-float tanque_diametro = 1;
-float tanque_altura = 1.5;
+// Medidas del tanque expresada en centimetros
+float tanque_diametro = 100;
+float tanque_altura = 150;
+float tanque_radio = tanque_diametro / 2;
 
 // inicializo objetos
 WiFiClient net;
@@ -60,29 +64,36 @@ void loop()
 {
   // Arduino OTA
   ArduinoOTA.handle();
-
   
-  // mido la distancia (se puede mas prolijo
-  duracion = sensor.ping_median();
+  // mido la distancia
+  duracion = sensor.ping_median(20);
   distancia = duracion / US_ROUNDTRIP_CM;
 
   // calculo los litros aprox
-  // desarrollar
-  litros = (M_PI * (tanque_diametro / 2) * (tanque_diametro /2) * (tanque_altura - (distancia / 100))) * 1000;
-
+  // Calculo la capacidad total del tanque con las medidas iniciales en cm del tanque.
+  float capacidad_total = M_PI * tanque_radio * tanque_radio * tanque_altura;
+  // Calculo el espacio vacío medido por el sensor usando las medidas iniciales en cm del tanque y 
+  // la distancia en cm calculada con el sensor.
+  float espacio_vacio =  M_PI * tanque_radio * tanque_radio * distancia;
+  // La resta de la capacidad total y el espacio vacío da en cm3 la cantidad de agua que tiene el tanque, 
+  // para pasar de cm3 a litros multiplico por 0.001 o divido por 1000, es lo mismo.
+  litros = (capacidad_total - espacio_vacio) * 0.001;
+  
   //uptime
   uptime = (millis() / 60000);
 
   // armo un mensaje json con todos los datos
-  sprintf(payload, "{\"uptime\":%u,\"distancia\":%g,\"litros\":%g}", uptime, distancia, litros);
+  sprintf(payload, "{\"uptime\":%u,\"distancia\":%g,\"litros\":%g,\"intervalo_actualizacion\":%i}", uptime, distancia, litros, LOOP_TIME);
 
   // muestro en el monitor
-  Serial.println(payload);
+  Serial.print(payload);
 
   // publico en el broker
   if (client.connect(mqtt_client, mqtt_user, mqtt_pwd)) {
     client.publish("wal", payload);
-    Serial.println("MQTT publicado");
+    Serial.println(" *");
+  } else {
+    Serial.println(" [mqtt push error]");
   }
-  delay(10 * 1000);
+  delay(LOOP_TIME * 1000);
 }
