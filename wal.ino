0065f3165e4628298533ce5c58ddcf9632fe1228
Mr Fussion <mgsilva@gmail.com>
2022-01-09 13:38:55 -0300

    agrego un if en lugar de delay(), para que continue el loop. Agrego light sleep. Modifico configuracion inicial a la real.

--- wal.ino
+++ wal.ino
@@ -1,4 +1,4 @@
-/* mgs 22-12-2-21 */
+/* mgs 22-12-21 */
 #include <NewPing.h>
 #include <ArduinoOTA.h>
 #include <ESP8266WiFi.h>
@@ -20,9 +20,9 @@ char mqtt_client[] = "wal";
 char mqtt_user[] = "";
 char mqtt_pwd[] = "";
 
-// Medidas del tanque expresada en centimetros
-float tanque_diametro = 100;
-float tanque_altura = 150;
+// Medidas del tanque expresada en centimetros (cm)
+float tanque_diametro = 111;
+float tanque_altura = 113.6728;
 float tanque_radio = tanque_diametro / 2;
 
 // inicializo objetos
@@ -33,6 +33,7 @@ NewPing sensor(PinTrig, PinEcho, 200);
 // Declaraciones
 float distancia, litros,capacidad_total, espacio_vacio, porcentaje_agua;
 unsigned long uptime;
+unsigned long previousMillis = 0;
 int duracion;
 char payload[110];
 
@@ -64,41 +65,58 @@ void loop()
 {
   // Arduino OTA
   ArduinoOTA.handle();
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= (LOOP_TIME * 1000) || previousMillis == 0)
+  {
+    // despierto el dispositivo
+    Serial.println("Despierta");
+    WiFi.setSleepMode(WIFI_NONE_SLEEP);
+    // espero porque espero, por las dudas
+    delay(1000);
+    
+    // mido la distancia
+    duracion = sensor.ping_median(20);
+    distancia = duracion / US_ROUNDTRIP_CM;
+
+    // calculo los litros aprox
+    // Calculo la capacidad total del tanque con las medidas iniciales en cm del tanque.
+    capacidad_total = M_PI * tanque_radio * tanque_radio * tanque_altura;
   
-  // mido la distancia
-  duracion = sensor.ping_median(20);
-  distancia = duracion / US_ROUNDTRIP_CM;
-
-  // calculo los litros aprox
-  // Calculo la capacidad total del tanque con las medidas iniciales en cm del tanque.
-  capacidad_total = M_PI * tanque_radio * tanque_radio * tanque_altura;
+    // Calculo el espacio vacío medido por el sensor usando las medidas iniciales en cm del tanque y 
+    // la distancia en cm calculada con el sensor.
+    espacio_vacio =  M_PI * tanque_radio * tanque_radio * distancia;
   
-  // Calculo el espacio vacío medido por el sensor usando las medidas iniciales en cm del tanque y 
-  // la distancia en cm calculada con el sensor.
-  espacio_vacio =  M_PI * tanque_radio * tanque_radio * distancia;
+    // La resta de la capacidad total y el espacio vacío da en cm3 la cantidad de agua que tiene el tanque, 
+    // para pasar de cm3 a litros multiplico por 0.001 o divido por 1000, es lo mismo.
+    litros = (capacidad_total - espacio_vacio) * 0.001;
+
+    // porcentaje de agua en el tanque
+    porcentaje_agua =  (capacidad_total - espacio_vacio) * 100 / capacidad_total;
   
-  // La resta de la capacidad total y el espacio vacío da en cm3 la cantidad de agua que tiene el tanque, 
-  // para pasar de cm3 a litros multiplico por 0.001 o divido por 1000, es lo mismo.
-  litros = (capacidad_total - espacio_vacio) * 0.001;
+    //uptime
+    uptime = (millis() / 60000);
 
-  // porcentaje de agua en el tanque
-  porcentaje_agua =  (capacidad_total - espacio_vacio) * 100 / capacidad_total;
-  
-  //uptime
-  uptime = (millis() / 60000);
+    // armo un mensaje json con todos los datos
+    sprintf(payload, "{\"uptime\":%u,\"distancia\":%g,\"litros\":%g,\"intervalo_actualizacion\":%i,\"porcentaje\":%g}", uptime, distancia, litros, LOOP_TIME, porcentaje_agua);
 
-  // armo un mensaje json con todos los datos
-  sprintf(payload, "{\"uptime\":%u,\"distancia\":%g,\"litros\":%g,\"intervalo_actualizacion\":%i,\"porcentaje\":%g}", uptime, distancia, litros, LOOP_TIME, porcentaje_agua);
+    // muestro en el monitor
+    Serial.print(payload);
 
-  // muestro en el monitor
-  Serial.print(payload);
+    // publico en el broker
+    if (client.connect(mqtt_client, mqtt_user, mqtt_pwd)) {
+      client.publish("wal", payload);
+      Serial.println(" *");
+    } else {
+      Serial.println(" [mqtt push error]");
+    }
 
-  // publico en el broker
-  if (client.connect(mqtt_client, mqtt_user, mqtt_pwd)) {
-    client.publish("wal", payload);
-    Serial.println(" *");
-  } else {
-    Serial.println(" [mqtt push error]");
+    // guardo el valor de tiempo
+    previousMillis = millis();
+
+    // Duermo el dispositivo
+    // WIFI_LIGHT_SLEEP WIFI_MODEM_SLEEP
+    Serial.println("Duerme");
+    WiFi.setSleepMode(WIFI_LIGHT_SLEEP);
   }
-  delay(LOOP_TIME * 1000);
 }
